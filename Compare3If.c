49c615d3834ec9e492ecf828a27f078f54f6a0e9
Jaisal Srivastava <jaisalsrivastava@gmail.com>
2019-05-07 14:56:34 +0530

    Update Compare3If.c

--- Compare3If.c
+++ Compare3If.c
@@ -1,23 +1,3 @@
-Skip to content
-Why GitHub? 
-Enterprise
-Explore 
-Marketplace
-Pricing 
-Search
-
-Sign in
-Sign up
-0 0 0 darshanshah262/skull1
- Code  Issues 0  Pull requests 0  Projects 0  Insights
-Join GitHub today
-GitHub is home to over 36 million developers working together to host and review code, manage projects, and build software together.
-
-skull1/compare3
-@darshanshah262 darshanshah262 Update compare3
-14a8e1f 5 hours ago
-43 lines (37 sloc)  482 Bytes
-  
 #include<stdio.h>
 int get()
 {
@@ -59,16 +39,3 @@ int main()
     return 0;
 }
 
-
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
