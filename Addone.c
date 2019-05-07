734e34d78e47cb1f78b42d0c134d7107f31c5169
Jaisal Srivastava <jaisalsrivastava@gmail.com>
2019-05-07 15:05:33 +0530

    AddOne Program Commit

---
+++ Addone.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int getdata()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    return a;
+}
+
+int logic(int a)
+{ 
+    int r;
+    r=a+1;
+    return r;
+}
+
+void output(int a, int r)
+{
+    printf("%d + 1 = %d",a,r);
+}
+
+int main()
+{
+    int a=getdata();
+    int r=logic(a);
+    output(a,r);
+}
