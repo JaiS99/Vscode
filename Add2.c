4f1bd75494685d5701edb0db486a61ff0341aefe
Jaisal Srivastava <48391316+JaiS99@users.noreply.github.com>
2019-05-15 02:14:37 +0530

    Update Add2.c

--- Add2.c
+++ Add2.c
@@ -1,28 +1,34 @@
 #include<stdio.h>
-int data()
+
+int Input()
 {
     int a;
-    printf("enter a number");
     scanf("%d",&a);
     return a;
 }
-int add(int a, int b)
+
+int Add(int a, int b)
 {
     int c;
     c=a+b;
     return c;
 
 }
-int op(int c)
+
+int Output(int c)
 {
-    printf("the sum is %d",c);
+    printf("The sum of both entered Numbers is:\n%d",c);
 }
+
 int main()
 {
-    int a,b,c;
-   a= data();
-   b= data();
-   c= add(a,b);
-    op(c);
-    return 0;
+   int a,b,c;
+   printf("enter 1st number \n");
+   a= Input();
+   printf("enter 2nd number \n");
+   b= Input();
+   
+   c= Add(a,b);
+   Output(c);
+   return 0;
 }
